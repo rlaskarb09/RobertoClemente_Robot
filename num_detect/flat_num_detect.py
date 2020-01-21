70b62373a817bfe9112872fdcfa7fa675bf092f2
sooah <soualee97@gmail.com>
2020-01-21 12:02:15 -0500

    modify for another img

--- num_detect/flat_num_detect.py
+++ num_detect/flat_num_detect.py
@@ -6,100 +6,47 @@ from bright.bright_function import edge_enhancement
 import imutils
 from imutils import contours
 from imutils.perspective import four_point_transform
+from num_detect.detect_function import detect, warp
 
-def detect(c, thresh):
-    # initialize the shape name and approximate the contour
-    shape = "unidentified"
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+# datafolder = '/Users/soua/Desktop/Project/sterling_demo'
+# imgpath = datafolder + '/Img_1077.jpg' # 201
 
-    # if the shape is a triangle, it will have 3 vertices
-    if len(approx) == 3:
-        shape = "triangle"
-
-    # if the shape has 4 vertices, it is either a square or
-    # a rectangle
-    elif len(approx) == 4:
-        # compute the bounding box of the contour and use the
-        # bounding box to compute the aspect ratio
-        (x, y, w, h) = cv2.boundingRect(approx)
-
-        # thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
-        kernel = np.ones((4, 4), np.uint8)
-        kernel2 = np.ones((5, 5), np.uint8)
-
-        thresh = cv2.erode(thresh, kernel, iterations=1)
-        thresh = cv2.dilate(thresh, kernel2, iterations=1)
-
-        sign = thresh[y:y + h, x:x + w]
-        sign = np.rot90(sign, 3)
-        thresh = cv2.threshold(sign, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 2))
-        # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-
-        ar = w / float(h)
-
-        # a square will have an aspect ratio that is approximately
-        # equal to one, otherwise, the shape is a rectangle
-        shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
-
-    # otherwise, we assume the shape is a circle
-    else:
-        shape = "octagon"
-    return shape, thresh
-
-def warp(cnt, orig):
-    pts = cnt.reshape(4,2)
-    rect = np.zeros((4, 2), dtype = 'float32')
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    (top_l, top_r, bot_r, bot_l) = rect
-    widthA = np.sqrt(((bot_r[0] - bot_l[0])**2) + ((bot_r[1] - bot_l[1])**2))
-    widthB = np.sqrt(((top_r[0] - top_l[0])**2) + ((top_r[1] - top_l[1])**2))
-
-    heightA = np.sqrt(((top_r[0] - bot_r[0])**2) + ((top_r[1] - bot_r[1])**2))
-    heightB = np.sqrt(((top_l[0] - bot_l[0])**2) + ((top_l[1] - bot_l[1])**2))
-
-    maxWidth = max(int(widthA), int(widthB))
-    maxHeight = max(int(heightA), int(heightB))
-
-    dst = np.array([[0,0], [maxWidth-1,0], [maxWidth-1, maxHeight-1], [0, maxHeight-1]], dtype="float32")
-
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
-    return warp
-
-datafolder = '/Users/soua/Desktop/Project/sterling_demo'
-imgpath = datafolder + '/Img_1077.jpg'
+datafolder = '/Users/soua/Desktop/Project/sterling_demo2'
+imgpath = datafolder + '/Img_45.jpg' # 102
+# imgpath = datafolder + '/Img_131.jpg' # 203
 
 img = cv2.imread(imgpath)
 img = edge_enhancement(img)
+cv2.imshow('origin', img)
 
 alpha = 1.5
-beta = 50
+beta = 30
 res = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
+cv2.imshow('scaling img', res)
+# cv2.waitKey(0)
 
 hsv = cv2.cvtColor(res, cv2.COLOR_BGR2HSV)
+cv2.imshow('hsv', hsv)
 h, s, v = cv2.split(hsv)
 v = 255 - v
-s = s + 50
+s = s + 50 # 102
+# s = s + 30
 hsv = cv2.merge((h, s, v))
 cv2.imshow('alpha beta hsv', hsv)
-
+# cv2.waitKey(0)
 # boundaries = [([60, 90, 80], [90, 255, 110])]  #GREEN
-boundaries = [([50, 80, 0], [90, 150, 20])]
+# boundaries = [([50, 80, 0], [90, 150, 20])] # sterling_demo 201
+boundaries = [([40,80,0], [70,255,80])] # sterling_demo 102
+
 lowerG = np.array(boundaries[0][0], dtype = 'uint8')
 upperG = np.array(boundaries[0][1], dtype = 'uint8')
 
 kernel = np.ones((3,3), np.uint8)
 mask = cv2.inRange(hsv, lowerG, upperG)
+cv2.imshow('mask', mask)
+# cv2.waitKey(0)
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+# cv2.waitKey(0)
 
 img_copy = img.copy()
 cnts, max_cont, approx_cnt = geom.find_main_contour(mask)
@@ -107,7 +54,7 @@ cv2.drawContours(img_copy, max_cont, -1, (255, 0, 0), 2)
 cv2.imshow('first contour', img_copy)
 cv2.drawContours(img_copy, [approx_cnt], -1, (0, 0, 255), 2)
 cv2.imshow('approx contour', img_copy)
-
+# cv2.waitKey(0)
 # img_copy = img.copy()
 # cv2.drawContours(img_copy, [box], -1, (255, 0, 0), 2)
 # cv2.imshow('box', img_copy)
@@ -143,32 +90,43 @@ cv2.imshow('warp image', warp)
 #     digits=[]
 #     print('%d digits detected'%len(digitcnts))
 warp_gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+warp_filt = cv2.adaptiveThreshold(warp_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
 cv2.imshow('warp gray', warp_gray)
-
+cv2.imshow('warp filtered', warp_filt)
 # warp_gray= edge_enhancement(warp_gray)
-thresh_ = cv2.threshold(warp_gray, 100, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+thresh_ = cv2.threshold(warp_gray, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+cv2.imshow('thresh', thresh_)
 # cv2.waitKey(0)
-thresh = 255 - thresh_
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel)
-cv2.imshow('thresh', thresh)
-# cv2.waitKey(0)
+# thresh = 255 - thresh_
+th_kernel = np.ones((2,2), np.uint8)
+# thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, th_kernel)
+thresh = cv2.morphologyEx(thresh_, cv2.MORPH_OPEN, th_kernel)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, th_kernel)
 
-thresh_cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+thresh_cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 thresh_cnts = imutils.grab_contours(thresh_cnts)
+
+cv2.imshow('thresh after morpho', thresh)
+cv2.waitKey(0)
 
 digitcnts = []
 for c in thresh_cnts:
     (x, y, w, h_) = cv2.boundingRect(c)
-    if w >= 15 and (h_ >= 20 and h_ <= 40):
+    if (h_ >= 20 and h_ <= 40):
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         digitcnts.append(box)
-    elif w < 10 and (h_ >= 20 and h_ <= 40):
-        rect = cv2.minAreaRect(c)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        digitcnts.append(box)
+    # if w >= 10 and (h_ >= 20 and h_ <= 40):
+    #     rect = cv2.minAreaRect(c)
+    #     box = cv2.boxPoints(rect)
+    #     box = np.int0(box)
+    #     digitcnts.append(box)
+    # elif w < 5 and (h_ >= 20 and h_ <= 40):
+    #     rect = cv2.minAreaRect(c)
+    #     box = cv2.boxPoints(rect)
+    #     box = np.int0(box)
+    #     digitcnts.append(box)
 
 cv2.drawContours(warp, digitcnts, -1, (255, 0, 0), 2)
 cv2.imshow('draw digit', warp)
@@ -176,21 +134,25 @@ cv2.waitKey(0)
 
 DIGITS_LOOKUP = {
     (1, 1, 1, 0, 1, 1, 1): 0,
+    (1, 1, 1, 1, 1, 1, 1): 0,
     # (1, 1, 1, 1, 1, 1, 1): 1,
     (0, 0, 1, 0, 0, 1, 0): 1,
+
     (1, 0, 1, 1, 1, 0, 1): 2,
+    (0, 0, 1, 1, 1, 0, 1): 2,
+
     (1, 0, 1, 1, 0, 1, 1): 3
 }
 
 digits = []
 for cc in digitcnts:
     (x, y, w, h) = cv2.boundingRect(cc)
-    if w < 10:
+    if w < 5:
         w = w+8
         x = x-8
     roi = thresh[y:y+h, x:x+w]
     (roiH, roiW) = roi.shape
-    (dW, dH) = (int(roiW * 0.3), int(roiH * 0.2))
+    (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
     dHC = int(roiH * 0.05)
     cv2.drawContours(warp, [cc], -1, (0, 0, 255), 2)
     cv2.imshow('digit', warp)
@@ -204,6 +166,15 @@ for cc in digitcnts:
         ((w - dW, h // 2), (w, h)),  # bottom-right
         ((0, h - dH), (w, h))  # bottom
     ]
+    # segments = [
+    #     ((w // 2, 0), (w//2, dH)), # top
+    #     ((0, h//4), (dW, h//4)), # top-left
+    #     ((w-dW, h//4), (w, h//4)), # top-right
+    #     ((w//2, h//2 - dH//2), (w//2, h//2 + dH//2)), # center
+    #     ((0, 3*h//4), (dW, 3*h//4)), # bottom-left
+    #     ((w-dW, 3*h//4), (w, 3*h//4)), # bottom-right
+    #     ((w//2, h-dH), (w//2, h)) # bottom
+    # ]
     on = [0] * len(segments)
     # loop over the segments
     for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
@@ -215,11 +186,14 @@ for cc in digitcnts:
         # cv2.waitKey(0)
         total = cv2.countNonZero(segROI)
         area = (xB - xA) * (yB - yA)
+        # length = (xB - xA) + (yB - yA)
 
         # if the total number of non-zero pixels is greater than
         # 50% of the area, mark the segment as "on"
-        if total / float(area) >= 0.6:
+        if total / float(area) > 0.50:
             on[i] = 1
+        # if total / float(length) >= 0.85:
+        #     on[i] = 1
 
     # lookup the digit and draw it on the image
     digit = DIGITS_LOOKUP[tuple(on)]

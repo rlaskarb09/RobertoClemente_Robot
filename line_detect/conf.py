22accdccd6b531839963601b5c190f75a2fb6e72
Chwaaaa <chwaaaa@chwaaaa.wifi.local.cmu.edu>
2020-01-22 11:59:42 -0500

    linetrack

---
+++ line_detect/conf.py
@@ -0,0 +1,25 @@
+## Picture settings
+# initial grayscale threshold
+threshold = 120
+# max grayscale threshold
+threshold_max = 180
+#min grayscale threshold
+threshold_min = 40
+# iterations to find balanced threshold
+th_iterations = 10
+# min % of white in roi
+white_min=3
+# max % of white in roi
+white_max=12
+
+#Driving setting
+rightCenter = 20
+rightAngle  = 90
+
+#PID setting
+kp = 1
+kd = 0
+# angle error
+angle_step = 0.6
+# shift error
+shift_step = 0.4
